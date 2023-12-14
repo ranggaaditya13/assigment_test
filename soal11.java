2bcfe4c4b679d81f0fd5a465b1dc7077521b9ada
ranggaaditya13 <lenovorangga@gmail.com>
2023-12-15 00:30:05 +0700

    update soal11

--- soal11.java
+++ soal11.java
@@ -4,15 +4,16 @@ public class soal11 {
 
 
     public static void main(String[] args) {
-        String nama = "jeruk";
-        int z = nama.length() / 2;
-        if ( nama.length() %2==0){
-            z +=1;
+        String nama = "afrika";
+        int z = middle(nama);
+        int up = 0;
+        if (nama.length() %2==0){
+            up +=1;
         }
-        System.out.println(z);
+
         for (int i=0;i<nama.length();i++){
-            for (int j=0;j<nama.length();j++){
-                if (j==z){
+            for (int j=0;j<nama.length()+up;j++){
+                if (j==z+up){
                     System.out.print(nama.charAt(i));
                 }else{
                     System.out.print("*");
@@ -21,6 +22,24 @@ public class soal11 {
             System.out.println();
         }
 
-
     }
+
+
+    public static int middle(String str)
+    {
+        int position;
+        int length;
+        if (str.length() % 2 == 0)
+        {
+            position = str.length() / 2 - 1;
+            length = 2;
+        }
+        else
+        {
+            position = str.length() / 2;
+            length = 1;
+        }
+        return position;
+    }
+
 }
